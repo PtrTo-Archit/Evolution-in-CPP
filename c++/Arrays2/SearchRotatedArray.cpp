59d4cbb6ea2a505b92e157b4fd0049dfcf19efd6
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-11 19:42:54 +0530

    Modified SearchROtated Problem

--- c++/Arrays2/SearchRotatedArray.cpp
+++ c++/Arrays2/SearchRotatedArray.cpp
@@ -5,15 +5,15 @@ using namespace std;
     int high=n-1;
     while(low<=high){
         
-        int mid=(low+high)/2;
+        int mid=low + (high-low)/2;
         if(arr[mid]==target){
             return mid;
         }
         //check if left of mid is sorted
         else if(arr[low]<=arr[mid]){
             // check if the target is present in left of mid
-            if(target>=arr[low] && target<=arr[mid]){
-                high=mid;
+            if(target>=arr[low] && target<arr[mid]){
+                high=mid-1;
             }
             else{
                 low=mid+1;
@@ -26,7 +26,7 @@ using namespace std;
                 low=mid+1;
             }
             else{
-                high = mid;
+                high = mid-1;
             }
         }
     }
@@ -35,13 +35,18 @@ using namespace std;
 int main(){
     int arr[100];
     int size;
-    int target;
-    cin>>size>>target;
+    int target=6;
+    cin>>size;
 
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
-    cout<<"Target is at index = "<<SearchRotatedArray(arr,size,target)<<endl;
+    int idx= SearchRotatedArray(arr,size,target);
+    if(idx==-1){
+        cout<<"Target not found"<<endl;
+    }
+    else{
+        cout<<"Target is at index = "<<SearchRotatedArray(arr,size,target)<<endl;
+    }
     return 0;
-    
 }
