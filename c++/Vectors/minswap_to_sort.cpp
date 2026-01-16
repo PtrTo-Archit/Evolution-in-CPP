a5c869cafccd090860b24f8100a87dbfd8474403
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-16 17:29:10 +0530

    MAde changes in minswap_to_sort

--- c++/Vectors/minswap_to_sort.cpp
+++ c++/Vectors/minswap_to_sort.cpp
@@ -2,26 +2,26 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-int minswaps(vector<int>&arr){
-    int n=arr.size();
+int min_swap_to_sort(int arr[],int n){
     vector<pair<int,int>>v;
     for(int i=0;i<n;i++){
         v.push_back({arr[i],i});
     }
     sort(v.begin(),v.end());
     int i=0;
-    int count=0;
+    int count =0;
     while(i<n){
         while(v[i].second!=i){
-            swap(v[i],v[v[i].second]);
             count++;
+            swap(v[i],v[v[i].second]);
         }
         i++;
     }
     return count;
-
 }
 int main(){
-vector<int>arr={2,10,1,5};
-cout<<minswaps(arr);
+    int arr[]={2,10,1,5};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<"Minimum swaps required : "<<min_swap_to_sort(arr,n);
+    return 0;
 }
