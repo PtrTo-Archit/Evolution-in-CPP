bc8a0f9d3bbcb2907adc42497571bb0a802d462c
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-17 19:30:05 +0530

    Added 1 more solution

--- c++/2Darrays.cpp/transpose_matrix.cpp
+++ c++/2Darrays.cpp/transpose_matrix.cpp
@@ -23,3 +23,35 @@ int main(){
     transpose(mat,2,3);
     return 0;
 }
+
+/* M->2
+#include<iostream>
+using namespace std;
+void transpose(int matrix[][3],int n,int m){
+    int transpose[m][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            transpose[j][i]=matrix[i][j];
+        }
+    }
+    cout<<"Original Matrix : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Transposed Matrix : "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<transpose[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return;
+}
+int main(){
+    int matrix[2][3]={{1,2,3},{4,5,6}};
+    transpose(matrix,2,3);
+    return 0;
+}*/
