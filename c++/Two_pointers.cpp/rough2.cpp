5f5585302fd4ad31ce0924bb97f43f1d7fbda9e0
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-27 23:35:52 +0530

    No changes

--- c++/Two_pointers.cpp/rough2.cpp
+++ c++/Two_pointers.cpp/rough2.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int main() {
-    int str[50]={'c','o','d','e','\0'};
-    cout<<(str)<<endl;
-    cout<<sizeof(str);
+bool Almostequal(string str1,string str2){
+    int count1[26]={0};
+    int count2[26]={0};
+    int diffcount=0;
+    if(str1==str2){
+        return true;
+    }
+    if(str1.length()!=str2.length()){
+        return false;
+    }
+    for(int i=0;i<str1.length();i++){
+        count1[str1[i]-'a']++;
+        count2[str2[i]-'a']++;
+        if(str1[i]!=str2[i]){
+            diffcount++;
+        }
+        if(diffcount>2)return false;
+    }
+    for(int i=0;i<26;i++){
+        if(count1[i]!=count2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    string str1="bank";
+    string str2="bink";
+    cout<<boolalpha<<Almostequal(str1, str2);
+    return 0;
+
 }
