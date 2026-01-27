c5d54ddf77ede2919fe5a408ceb325cf885c9648
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-27 23:43:49 +0530

    Important note file added

---
+++ c++/strings.cpp/important_note.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char work[]="code";
+    cout<<work<<endl;
+    work[0]='m';
+    cout<<work<<endl;
+    const char *str="code";
+    cout<<str;
+
+}
