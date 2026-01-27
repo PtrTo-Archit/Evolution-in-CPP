62a9c6cad0eaf64a35334b1b9d0b3c45d8bdbc96
PtrTo-Archit <architsrivastava970@gmail.com>
2026-01-27 23:45:22 +0530

    new lines added

--- c++/strings.cpp/important_note.cpp
+++ c++/strings.cpp/important_note.cpp
@@ -5,7 +5,9 @@ int main(){
     cout<<work<<endl;
     work[0]='m';
     cout<<work<<endl;
+    //here in case of pointer it is only a read only memory we cannot modify it to mode
     const char *str="code";
     cout<<str;
 
 }
+// cin ignores whitespaces 
