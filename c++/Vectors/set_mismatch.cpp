55e627bc670d7c269cdf9f7ace86cd7a1185ba58
PtrTo-Archit <architsrivastava970@gmail.com>
2026-03-26 03:39:43 +0530

    New solutions added in SetMismatch

--- c++/Vectors/set_mismatch.cpp
+++ c++/Vectors/set_mismatch.cpp
@@ -30,3 +30,61 @@ int main(){
     }
     return 0;
 }
+// M-2 
+// Time Complexity - O(nlogn)
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// vector<int> setMismatch(vector<int>nums){
+//     sort(nums.begin(),nums.end());
+//     int dup=-1;
+//     int missing=1;
+//     for(int i=0;i<nums.size();i++){
+//         if(i>0 && nums[i]==nums[i-1]){
+//             dup=nums[i];
+//         }
+//         else if(nums[i]==missing){
+//             missing++;
+//         }
+//     }
+//     return {dup,missing};
+// }
+// int main(){
+//     vector<int>nums={3,2,1,1};
+
+//     vector<int>set=setMismatch(nums);
+//     cout<<"["<<set[0]<<","<<set[1]<<"]"<<endl;
+//     return 0;
+// }
+//M-3
+//Time Complexity O(n)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int>setMismatch(vector<int>nums){
+//     int n=nums.size();
+//     int duplicate =-1;
+//     int missing = -1;
+//     for(int i=0;i<n;i++){
+//         if(nums[abs(nums[i])-1]<0){
+//             duplicate=abs(nums[i]);
+//         }
+//         else {
+//             nums[abs(nums[i])-1]*=(-1);
+//         }
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>0){
+//                 missing = i+1;
+//                 break;
+//             }
+//         }
+//     }
+//     return {duplicate , missing};
+// }
+// int main(){
+//     vector<int>nums={1,3,3,4};
+//      vector<int>set=setMismatch(nums);
+//     cout<<"["<<set[0]<<","<<set[1]<<"]"<<endl;
+//     return 0;
+// }
