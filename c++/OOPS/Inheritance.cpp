e8c6d5a1f76df2b11428e4a04949641149b707d9
PtrTo-Archit <architsrivastava970@gmail.com>
2026-04-10 10:18:49 +0530

    Added example to Inheritance file

--- c++/OOPS/Inheritance.cpp
+++ c++/OOPS/Inheritance.cpp
@@ -28,3 +28,42 @@ f1.eat();
 f1.breathe();
 return 0;
 }
+// Example to understand protected and private inheritance.....
+
+//  #include<iostream>
+// #include<string>
+// using namespace std;
+// class Animal {
+//     public:
+//     void eats(){
+//         cout<<"Eats"<<endl;
+//     }
+//       void breathes(){
+//         cout<<"Breathes"<<endl;
+//     }
+// };
+// class Mammal: private Animal{
+// public:
+//     string bloodType;
+//     Mammal(){
+//         eats();
+//         bloodType="worm";
+//     }
+// };
+// class Dog: public Mammal{
+// public:
+// void Tailwag(){
+//     breathes();
+//     cout<<"Dog wags its tail"<<endl;
+// }
+// };
+// int main(){
+//     Dog d1;
+//     d1.Tailwag();
+//     // m1.eats();
+//     // m1.breathes();
+//     return 0;
+    
+
+// }
+
