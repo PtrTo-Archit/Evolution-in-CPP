1d5e8e7b648ef3eac09d6bf43e842d93ad46764f
PtrTo-Archit <architsrivastava970@gmail.com>
2026-04-10 12:15:08 +0530

    Example added in Abstract class file

--- c++/OOPS/abstract_class.cpp
+++ c++/OOPS/abstract_class.cpp
@@ -27,3 +27,38 @@ int main() {
     squ1.draw();
     return 0;
 }
+
+//Why abstract class is important..
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// // Abstract class
+// class Shape{
+//     public:
+//     virtual void draw()=0;  // -> Pure virtual function
+// };
+// class Circle: public Shape {
+//     public:
+//     void draw() {
+//        cout<<"draw circle\n"; 
+//     }
+
+// };
+// class Square: public Shape {
+//     public:
+//     void draw() {
+//        cout<<"draw square\n"; 
+//     }
+
+// };
+// int main() {
+// Shape* classroom[2]; // A single list that holds ANY shape
+// classroom[0] = new Circle();
+// classroom[1] = new Square();
+
+// for(int i = 0; i < 2; i++) {
+//     classroom[i]->draw(); // This works for ALL of them!
+// }
+// return 0;
+// }
