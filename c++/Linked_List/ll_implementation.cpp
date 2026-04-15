3faf3c3768f77e3fb9f485174acb9e64ed5dd897
PtrTo-Archit <architsrivastava970@gmail.com>
2026-04-16 01:33:33 +0530

    Added new folder linked list

---
+++ c++/Linked_List/ll_implementation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+class List{
+    public:
+    Node * head;
+    Node * tail;
+    List(){
+        head= NULL;
+        tail= NULL;
+    }
+};
+
+int main(){
+    List ll;
+    return 0;
+}
