23ab559bdef783dcaf70082a0dec7d9468ed310c
PtrTo-Archit <architsrivastava970@gmail.com>
2026-04-17 21:07:38 +0530

    new file recursive_search added

---
+++ c++/Linked_List/recursive_search.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+    ~Node(){
+        cout<<"node destructor for data = "<<data<<endl;
+        if(next!=NULL){
+            delete next;
+            next=NULL;
+        }
+    }
+};
+
+class List{
+    public:
+    Node * head;
+    Node * tail;
+    List(){
+        head= NULL;
+        tail= NULL;
+    }
+    void push_front(int val){
+        Node * newNode= new Node(val);
+        if(head!=NULL){
+            newNode->next=head;
+            head=newNode;
+        }
+        else{
+            head=tail=newNode;
+        }
+    }
+
+    void print(){
+        Node * temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+
+    ~List(){
+        cout<<"destructor of list"<<endl;
+        if(head!=NULL){
+            delete head;
+            head=NULL;
+        }
+    }
+          void pop_front(){
+            Node* temp=head;
+            if(head==NULL){
+                return;
+            }
+            head=head->next;
+            temp->next=NULL;
+            // For single node we need below condition
+            if(head==NULL){
+                tail=NULL;
+            }
+            delete temp;
+            
+        }
+        void pop_back(){
+            if(head==NULL){
+                return;
+            }
+            // For single node
+            if(head->next==NULL){
+                delete head;
+                head=tail=NULL;
+                return;
+            }
+            //for multiple nodes
+            Node* temp=head;
+            while(temp->next!=tail){
+                temp=temp->next;
+            }
+            temp->next=NULL;
+            delete tail;
+            tail=temp;
+        }
+        // int recursive_search(Node* temp, int key,int idx){
+        //     if(temp==NULL){
+        //         return -1;
+        //     }
+        //     if(temp->data==key){
+        //         return idx;
+        //     }
+        //     return recursive_search(temp->next,key, idx+1);
+        // }
+        // M-2 for recursive search when head access is not to main
+        int helper(Node * temp , int key){
+            if(temp==NULL){
+                return -1;
+            }
+            if(temp->data==key){
+                return 0;
+            }
+            int idx= helper(temp->next, key);
+            if(idx==-1){
+                return -1;
+            }
+            return idx+1;
+        }
+        int recursive_search(int key){
+        return helper(head,key);
+        }
+};
+
+int main(){
+    List ll;
+    ll.push_front(5);
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.print();
+    cout<<ll.recursive_search(4)<<endl;
+
+    
+    return 0;
+}
