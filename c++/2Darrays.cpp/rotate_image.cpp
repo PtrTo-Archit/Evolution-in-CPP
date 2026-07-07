9be43fbc81360feda5b362537f8ee0b1ae0bea9f
PtrTo-Archit <architsrivastava970@gmail.com>
2026-07-07 12:32:35 +0530

    new file rotate image added

---
+++ c++/2Darrays.cpp/rotate_image.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void print(vector<vector<int>>&matrix){
+    int n=matrix.size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return ;
+
+}
+void rotate(vector<vector<int>>&matrix){
+    int n=matrix.size();
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+}
+int main(){
+    vector<vector<int>>matrix={{1,2,3},{4,5,6},{7,8,9}};
+    rotate(matrix);
+    print(matrix);
+    return 0;
+
+}
