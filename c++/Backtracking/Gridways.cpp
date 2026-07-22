893861591d617922d12459bdfb021c9419e9fa7b
PtrTo-Archit <architsrivastava970@gmail.com>
2026-07-22 17:19:20 +0530

    Added new method of gridways

--- c++/Backtracking/Gridways.cpp
+++ c++/Backtracking/Gridways.cpp
@@ -1,18 +1,46 @@
+// M-1 Time complexity -O(2^(m+n))
+
+// #include<iostream>
+// using namespace std;
+// int GridWays(int r,int c, int n,int m){
+//     if(r==n-1 && c==m-1){
+//         return 1;
+//     }
+//     if(r>=n || c>=m){
+//         return 0;
+//     }
+//     int val1=GridWays(r,c+1,n,m);
+//     int val2=GridWays(r+1,c,n,m);
+//     return val1 + val2;
+// }
+// int main() {
+//     int n=3;
+//     int m=3;
+//    cout<<"Total Ways : "<< GridWays(0,0,n,m);
+// }
+
+//M-2 Using nCR
 #include<iostream>
 using namespace std;
-int GridWays(int r,int c, int n,int m){
-    if(r==n-1 && c==m-1){
-        return 1;
+int nCr(int n,int r){
+    if(r>n-r){
+        r=n-r;
     }
-    if(r>=n || c>=m){
-        return 0;
+
+    long long ans=1;
+    for(int i=1;i<=r;i++){
+        ans=ans*(n-r+i)/i;
     }
-    int val1=GridWays(r,c+1,n,m);
-    int val2=GridWays(r+1,c,n,m);
-    return val1 + val2;
+    return ans;
 }
-int main() {
+int gridWays(int n,int m){
+   long long count=nCr(n+m-2,n-1);
+   return count;
+}
+int main(){
     int n=3;
     int m=3;
-   cout<<"Total Ways : "<< GridWays(0,0,n,m);
+    int count=gridWays(n,m);
+    cout<<count;
+    return 0;
 }
