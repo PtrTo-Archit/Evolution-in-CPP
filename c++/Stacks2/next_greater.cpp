c1f6e0a454a86abd1e9ea35b7f536eaf89a6ee5a
PtrTo-Archit <architsrivastava970@gmail.com>
2026-08-24 06:51:47 +0530

    1 more solution added in next_greater file

--- c++/Stacks2/next_greater.cpp
+++ c++/Stacks2/next_greater.cpp
@@ -1,25 +1,55 @@
+// #include<iostream>
+// #include<vector>
+// #include<stack>
+// using namespace std;
+// vector<int> next_greater(vector<int>& arr){
+//     stack<int>s;
+//     int n=arr.size();
+//     vector<int> ans(n,0);
+//     for(int i=n-1;i>=0;i--){
+//         while(!s.empty() && arr[i]>=s.top()){
+//             s.pop();
+//         }
+//         ans[i]=s.empty()? -1:s.top();
+//         s.push(arr[i]);
+//     }
+//     return ans;
+// }
+// int main(){
+//     vector<int> arr={6,8,0,1,3};
+//     vector<int> ans=next_greater(arr);
+//     for(int val: ans){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+// }
 #include<iostream>
 #include<vector>
 #include<stack>
 using namespace std;
-vector<int> next_greater(vector<int>& arr){
+void next(vector<int>& arr, vector<int>&ans){
     stack<int>s;
-    int n=arr.size();
-    vector<int> ans(n,0);
+    int n= arr.size();
+    s.push(arr[n-1]);
+    ans[n-1]=-1;
     for(int i=n-1;i>=0;i--){
-        while(!s.empty() && arr[i]>=s.top()){
+        int curr=arr[i];
+        while(!s.empty() && curr>=s.top()){
             s.pop();
         }
-        ans[i]=s.empty()? -1:s.top();
+        ans[i]=s.empty() ? -1 : s.top();
         s.push(arr[i]);
     }
-    return ans;
+    for(int i=0;i<arr.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+
 }
 int main(){
     vector<int> arr={6,8,0,1,3};
-    vector<int> ans=next_greater(arr);
-    for(int val: ans){
-        cout<<val<<" ";
-    }
-    cout<<endl;
+    vector<int> ans(5,0);
+    next(arr,ans);
+    return 0;
+
 }
