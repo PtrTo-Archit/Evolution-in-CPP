1fc9988c784cce077e2cde4762303e3e2a5bc0ba
PtrTo-Archit <architsrivastava970@gmail.com>
2026-08-30 01:52:29 +0530

    two more solutions added

---
+++ c++/Queues/first_non_rep_letter.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+void fnrl(string str){
+    int freq[26]={0};
+    queue<char>q;
+    for(int i=0;i<str.size();i++){
+        char ch=str[i];
+        q.push(ch);
+        freq[ch-'a']++;
+        while(!q.empty() && freq[q.front()-'a']>1){
+            q.pop();
+        }
+        if(q.empty()){
+            cout<<"-1\n";
+        }
+        else{
+            cout<<q.front()<<endl;
+        }
+    }
+}
+int main(){
+    string str="aabccxb";
+    fnrl(str);
+    return 0;
+}
